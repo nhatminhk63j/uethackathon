bed0eb01453e59860eaf3c020c7c4aa377e957d7
Nhật Ngô Sách <nhat.ns@teko.vn>
2022-04-10 14:05:48 +0700

    add menu

--- index.js
+++ index.js
@@ -63,3 +63,41 @@ function updateSoftwareEngineeringTimer() {
     `<a class="register-now" href="" target="_blank" rel="noopener noreferrer">ĐĂNG KÝ NGAY</a>`;
 }
 setInterval("updateSoftwareEngineeringTimer()", 1000);
+
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 650 ||
+    document.documentElement.scrollTop > 650
+  ) {
+    document.getElementById("navbar").style.display = "block";
+  } else {
+    document.getElementById("navbar").style.display = "block";
+  }
+}
+
+$(document).ready(function () {
+  $('a[href^="#"]').on("click", function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $("html, body")
+      .stop()
+      .animate(
+        {
+          scrollTop: $target.offset().top,
+        },
+        1000,
+        "swing",
+        function () {
+          window.location.hash = target;
+        }
+      );
+  });
+});
